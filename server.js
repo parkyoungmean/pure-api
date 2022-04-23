034afa396e7e31cbaf17ed8cc1ee672139cea8f5
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2022-04-24 04:21:44 +0900

    01-02(09-02) Backend's first commit

---
+++ server.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const express = require('express');
+const notion = require('./notion');
+const cors = require('cors');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 4000;
+const HOST = "localhost";
+
+const databaseId = "635a00b0e6614673ab156a43f7a40524";
+
+// POST request
+// POST name, phhoneNumber, extraInfo
+// Functionality: Make a database entry in a Notion page with the databaseId above
+// localhost:4000/submitFormToNotion
+app.post('/submitFormToNotion', jsonParser, async (req, res) => {
+    // req.body
+    /* {
+        name: "peter",
+        phoneNumber: "000-0000-0000",
+        extraInfo: "this is Infomation",
+    } */
+    const name = req.body.name;
+    const phoneNumber = req.body.phoneNumber;
+    const extraInfo = req.body.extraInfo;
+
+    try {
+        const response = await notion.pages.create({
+            parent: { database_id: databaseId },
+            properties: {
+                Name: {
+                    title: [
+                        {
+                            text: {
+                                content: name
+                            }
+                        }
+                    ]
+                },
+                "Phone Number": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: phoneNumber
+                            }
+                        }
+                    ]
+                },
+                "Extra Information": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: extraInfo
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+        console.log(response);
+        console.log("SUCCESS!")
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.listen(PORT, HOST, () => {
+    console.log("Starting proxy at " + HOST + ":" + PORT);      // localhost:4000
+})
