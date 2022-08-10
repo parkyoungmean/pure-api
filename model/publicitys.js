19419825d49cb3d9102caf5b094478a220d1ab95
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2022-08-10 22:02:12 +0900

    03-05 publicities: make updatePublicity router

--- model/publicitys.js
+++ model/publicitys.js
@@ -2,186 +2,346 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const database_id = process.env.NOTICES_DATABASE_ID;
 
-
 const getDatabase = async () => {
-    const response = await notion.databases.retrieve({
-        database_id: database_id,
-    });
-    console.log(response);
-}
+  const response = await notion.databases.retrieve({
+    database_id: database_id,
+  });
+  console.log(response);
+};
 
 /* getDatabase(); */
 
 const getPublicitys = async () => {
-    const payload = {
-      path: `databases/${database_id}/query`,
-      method: `POST`,
-      body: {
-        sorts: [
-          {
-            property: "CreatedAt",
-            direction: "descending",
-          },
-        ],
-        filter: {
-          or: [{ property: "Status", rich_text: { does_not_contain: "deleted" }}],
+  const payload = {
+    path: `databases/${database_id}/query`,
+    method: `POST`,
+    body: {
+      sorts: [
+        {
+          property: "CreatedAt",
+          direction: "descending",
         },
+      ],
+      filter: {
+        or: [
+          { property: "Status", rich_text: { does_not_contain: "deleted" } },
+        ],
       },
+    },
+  };
+  const { results } = await notion.request(payload);
+
+  const publicitys = results.map((page) => {
+    return {
+      id: page.id,
+      Title: page.properties.Title.title[0].text.content,
+      Subtitle: page.properties.Subtitle.rich_text[0].text.content,
+      Content: page.properties.Content.rich_text[0].text.content,
+      Img: page.properties.Img.rich_text[0].text.content,
+      MobileImg: page.properties.MobileImg.rich_text[0].text.content,
+      Condition: page.properties.Condition.rich_text[0].text.content,
+      Belong: page.properties.Belong.rich_text[0].text.content,
+      Author: page.properties.Author.rich_text[0].text.content,
+      Position: page.properties.Position.rich_text[0].text.content,
+      Size: page.properties.Size.rich_text[0].text.content,
+      Color: page.properties.Color.rich_text[0].text.content,
+      CreatedAt: page.properties.CreatedAt.date.start,
+      UpdatedAt: page.properties.UpdatedAt.date.start,
+      Status: page.properties.Status.rich_text[0].text.content,
     };
-    const { results } = await notion.request(payload);
-  
-    const publicitys = results.map((page) => {
-      return {
-        id: page.id,
-        Title: page.properties.Title.title[0].text.content,
-        Subtitle: page.properties.Subtitle.rich_text[0].text.content,
-        Content: page.properties.Content.rich_text[0].text.content,
-        Img: page.properties.Img.rich_text[0].text.content,
-        MobileImg: page.properties.MobileImg.rich_text[0].text.content,
-        Condition: page.properties.Condition.rich_text[0].text.content,
-        Belong: page.properties.Belong.rich_text[0].text.content,
-        Author: page.properties.Author.rich_text[0].text.content,
-        Position: page.properties.Position.rich_text[0].text.content,
-        Size: page.properties.Size.rich_text[0].text.content,
-        Color: page.properties.Color.rich_text[0].text.content,
-        CreatedAt: page.properties.CreatedAt.date.start,
-        UpdatedAt: page.properties.UpdatedAt.date.start,
-        Status: page.properties.Status.rich_text[0].text.content,
-      };
-    });
-  
-    return publicitys;
+  });
+
+  return publicitys;
 };
 
-const createPublicity = async (img, mobileImg, title, subtitle, content, condition, belong, author, position, size, color, createdAt, updatedAt) => {
-    const response = await notion.pages.create({
-        parent: { database_id: database_id },
-        properties: {
-            Title: {
-                title: [
-                {
-                    text: {
-                        content: title,
-                    },
-                },
-                ],
+const createPublicity = async (
+  img,
+  mobileImg,
+  title,
+  subtitle,
+  content,
+  condition,
+  belong,
+  author,
+  position,
+  size,
+  color,
+  createdAt,
+  updatedAt
+) => {
+  const response = await notion.pages.create({
+    parent: { database_id: database_id },
+    properties: {
+      Title: {
+        title: [
+          {
+            text: {
+              content: title,
             },
-            Img: {
-                rich_text: [
-                {
-                    text: {
-                        content: img,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Img: {
+        rich_text: [
+          {
+            text: {
+              content: img,
             },
-            MobileImg: {
-                rich_text: [
-                {
-                    text: {
-                        content: mobileImg,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      MobileImg: {
+        rich_text: [
+          {
+            text: {
+              content: mobileImg,
             },
-            Subtitle: {
-                rich_text: [
-                {
-                    text: {
-                        content: subtitle,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Subtitle: {
+        rich_text: [
+          {
+            text: {
+              content: subtitle,
             },
-            Content: {
-                rich_text: [
-                {
-                    text: {
-                        content: content,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Content: {
+        rich_text: [
+          {
+            text: {
+              content: content,
             },
-            Condition: {
-                rich_text: [
-                {
-                    text: {
-                        content: condition,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Condition: {
+        rich_text: [
+          {
+            text: {
+              content: condition,
             },
-            Belong: {
-                rich_text: [
-                {
-                    text: {
-                        content: belong,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Belong: {
+        rich_text: [
+          {
+            text: {
+              content: belong,
             },
-            Author: {
-                rich_text: [
-                {
-                    text: {
-                        content: author,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Author: {
+        rich_text: [
+          {
+            text: {
+              content: author,
             },
-            Position: {
-                rich_text: [
-                {
-                    text: {
-                        content: position,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Position: {
+        rich_text: [
+          {
+            text: {
+              content: position,
             },
-            Size: {
-                rich_text: [
-                {
-                    text: {
-                        content: size,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Size: {
+        rich_text: [
+          {
+            text: {
+              content: size,
             },
-            Color: {
-                rich_text: [
-                {
-                    text: {
-                        content: color,
-                    },
-                },
-                ],
+          },
+        ],
+      },
+      Color: {
+        rich_text: [
+          {
+            text: {
+              content: color,
             },
-            CreatedAt: {
-                date: {
-                    start: createdAt,
-                },
-            },
-            UpdatedAt: {
-                date: {
-                    start: updatedAt,
-                },
-            },
-            Status: {
-                rich_text: [
-                {
-                    text: {
-                    content: "created",
-                    },
-                },
-                ],
-            },
+          },
+        ],
+      },
+      CreatedAt: {
+        date: {
+          start: createdAt,
         },
-    });
+      },
+      UpdatedAt: {
+        date: {
+          start: updatedAt,
+        },
+      },
+      Status: {
+        rich_text: [
+          {
+            text: {
+              content: "created",
+            },
+          },
+        ],
+      },
+    },
+  });
 
-    return response;
-}
+  return response;
+};
+
+const updateSchool = async (
+  id,
+  img,
+  mobileImg,
+  title,
+  subtitle,
+  content,
+  condition,
+  belong,
+  author,
+  position,
+  size,
+  color,
+  createdAt,
+  updatedAt
+) => {
+  const response = await notion.pages.update({
+    parent: { database_id: database_id },
+    page_id: id,
+    properties: {
+      Title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Img: {
+        rich_text: [
+          {
+            text: {
+              content: img,
+            },
+          },
+        ],
+      },
+      MobileImg: {
+        rich_text: [
+          {
+            text: {
+              content: mobileImg,
+            },
+          },
+        ],
+      },
+      Subtitle: {
+        rich_text: [
+          {
+            text: {
+              content: subtitle,
+            },
+          },
+        ],
+      },
+      Content: {
+        rich_text: [
+          {
+            text: {
+              content: content,
+            },
+          },
+        ],
+      },
+      Condition: {
+        rich_text: [
+          {
+            text: {
+              content: condition,
+            },
+          },
+        ],
+      },
+      Belong: {
+        rich_text: [
+          {
+            text: {
+              content: belong,
+            },
+          },
+        ],
+      },
+      Author: {
+        rich_text: [
+          {
+            text: {
+              content: author,
+            },
+          },
+        ],
+      },
+      Position: {
+        rich_text: [
+          {
+            text: {
+              content: position,
+            },
+          },
+        ],
+      },
+      Size: {
+        rich_text: [
+          {
+            text: {
+              content: size,
+            },
+          },
+        ],
+      },
+      Color: {
+        rich_text: [
+          {
+            text: {
+              content: color,
+            },
+          },
+        ],
+      },
+      CreatedAt: {
+        date: {
+          start: createdAt,
+        },
+      },
+      UpdatedAt: {
+        date: {
+          start: updatedAt,
+        },
+      },
+      Status: {
+        rich_text: [
+          {
+            text: {
+              content: "updated",
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  return response;
+};
 
 module.exports = {
-    getPublicitys,
-    createPublicity,
-}
+  getPublicitys,
+  createPublicity,
+  updatePublicity,
+};
--- routes/publicity.js
+++ routes/publicity.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json())
 
-const { getPublicitys, createPublicity } = require('../model/publicitys');
+const { getPublicitys, createPublicity, updatePublicity } = require('../model/publicitys');
 
 router.get('/', async (req, res) => {
     const publicitys = await getPublicitys();
@@ -45,6 +45,43 @@ router.post('/createPublicity', async (req, res) => {
 
         console.log('결과:', response);
         console.log('CREATE SUCCESS PUBLICITY!');
+        res.json(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+/* Update Publicity - 슬라이드 광고 내용 수정을 위한 메서드 */
+router.post('/updatePublicity', async (req, res) => {
+    console.log('여기는 광고내용 수정 라우터입니다.');
+    console.log(req.body);
+
+    const id = req.body.id;
+
+    const img = req.body.img;
+    const mobileImg = req.body.mobileImg;
+    
+    const title = req.body.title;
+    const subtitle = req.body.subtitle;
+    const content = req.body.description;
+    const condition = req.body.condition;                    // 광고 or 공지
+    const belong = req.body.belong;                                    // 전체 or ~학교
+    const author = req.body.author;
+    const position = JSON.stringify(req.body.position);      // 전체 글의 x, y의 위치
+    const size = JSON.stringify(req.body.size);              // 전체 글의 글자 크기
+    const color = JSON.stringify(req.body.color);            // 전체 글의 글자색
+    const createdAt = req.body.createdAt;
+    const updatedAt = req.body.updatedAt;
+
+    console.log('img 파일 내용:', img);
+    console.log('mobileImg 파일 내용:', mobileImg);
+    
+    try {
+        const response = await updatePublicity(id, img, mobileImg, title, subtitle, content, condition, belong, author, position, size, color, createdAt, updatedAt,);
+
+        console.log(response);
+        console.log('UPDATE SUCCESS Publicity!');
         res.json(response);
     } catch (error) {
         console.error(error);
